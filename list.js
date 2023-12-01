ac2a7bb5467d3a8e534dd34e68d5a7a045db0524
Yvonne213 <hywener@gmail.com>
2023-12-01 08:48:56 -0600

    .

---
+++ list.js
@@ -0,0 +1,37 @@
+
+//-----------------------------------------------------
+let generatedWindow; // Variable to store the reference to the opened window
+var ws = new WebSocket("ws://192.168.194.100:8765");
+
+ws.onmessage = function (event) {
+  // trigger the message update
+  console.log("sentence received! do something here!");
+  console.log(event.data);
+  generatedSentence = event.data;
+
+  // Add the generated sentence to the history
+  sentenceHistory.push(generatedSentence);
+
+  // Check if the current page is the third HTML page
+  const isThirdHTML = window.location.href.includes("list.html");
+
+
+  // Update the sentence history list
+  const historyList = document.getElementById("history");
+  if (historyList) {
+    historyList.innerHTML = '';
+  }
+  sentenceHistory.forEach((sentence, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = sentence;
+
+    historyList.appendChild(listItem);
+  });
+
+  historyList.scrollTop = historyList.scrollHeight;
+  console.log(historyList.scrollHeight);
+};
+
+const sentenceHistory = [];
+
+
--- main.js
+++ main.js
@@ -71,7 +71,7 @@ async function main() {
   // connectButton.setAttribute("disabled", "true");
   //...............up is connection issues......................................//
 
-   // MetaMask is our 'provider' in this case
+  // MetaMask is our 'provider' in this case
   // const provider = new ethers.providers.Web3Provider(window.ethereum);
 
   // You (whoever is signed into MetaMask) is the 'signer'
@@ -93,8 +93,6 @@ async function main() {
 
   // ADD CODE TO INTERACT WITH THE CONTRACT
 
-
-
 }
 
 
@@ -104,36 +102,42 @@ var ws = new WebSocket("ws://192.168.194.100:8765");
 
 ws.onmessage = function (event) {
   // trigger the message update
-  console.log("sentence recieved! do something here!");
+  console.log("sentence received! do something here!");
   console.log(event.data);
   generatedSentence = event.data;
- //DO SOMETHING HERE 
 
   // Add the generated sentence to the history
   sentenceHistory.push(generatedSentence);
 
-  // Update the terminal with the latest sentence
-  const terminal = document.getElementById("terminal");
-  terminal.textContent = generatedSentence;
+  // Check if the current page is the third HTML page
+  const isThirdHTML = window.location.href.includes("list.html");
+
+  // Update the terminal with the latest sentence (on other pages)
+  if (!isThirdHTML) {
+    const terminal = document.getElementById("terminal");
+    terminal.textContent = generatedSentence;
+    terminal.style.color = "red"
+  }
 
   // Update the sentence history list
-  
   const historyList = document.getElementById("history");
-  historyList.innerHTML = '';
+  if (historyList) {
+    historyList.innerHTML = '';
+  }
   sentenceHistory.forEach((sentence, index) => {
     const listItem = document.createElement("li");
     listItem.textContent = sentence;
 
     historyList.appendChild(listItem);
-
   });
+
   historyList.scrollTop = historyList.scrollHeight;
   console.log(historyList.scrollHeight);
 };
 
 
 // ---------------------------------------------------------------------
-function sendMessage(content_content){
+function sendMessage(content_content) {
   console.log("send out ws message");
   ws.send(content_content);
 }
@@ -158,7 +162,6 @@ let times = [
 ];
 
 const sentenceHistory = [];
-
 // Function to generate a sentence based on user input
 function generateUserSentence() {
   const verbInput = document.getElementById("verbInput").value;
@@ -192,21 +195,21 @@ function generateUserSentence() {
       listItem.textContent = sentence;
 
       historyList.appendChild(listItem);
-      
+
       historyList.scrollTop = historyList.scrollHeight;
-  console.log(historyList.scrollHeight);
+      console.log(historyList.scrollHeight);
     });
 
-     // Clear the input fields
-  document.getElementById("verbInput").value = '';
-  document.getElementById("nounInput").value = '';
-  document.getElementById("timeInput").value = '';
+    // Clear the input fields
+    document.getElementById("verbInput").value = '';
+    document.getElementById("nounInput").value = '';
+    document.getElementById("timeInput").value = '';
 
   }
   // Generate the sentence
   const generatedSentence = `${line1}\n${line2}\n${line3}\n${line4}`;
 
- 
+
   // Open the new window and set its location to new.html with the generated content as a query parameter
   //const newWindow = window.open(`new.html?content=${encodeURIComponent(generatedSentence)}`, '_blank');
   sendMessage(generatedSentence);
@@ -217,6 +220,7 @@ function generateUserSentence() {
 
 // Function to generate a random sentence
 function generateRandomSentence() {
+
   const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
   const randomTime = times[Math.floor(Math.random() * times.length)];
@@ -231,30 +235,33 @@ function generateRandomSentence() {
 
   sendMessage(generatedSentence);
 
-  
 }
 
+const displayButton = document.getElementById("display-button");
 // Add click event listener to the "Display Sentence" button
-document.getElementById("display-button").addEventListener("click", generateUserSentence);
+if (displayButton) {
+  displayButton.addEventListener("click", generateUserSentence);
+  // Add click event listener to the "Generate Sentence" button
+  document.getElementById("generate-button").addEventListener("click", generateRandomSentence);
+}
 
-// Add click event listener to the "Generate Sentence" button
-document.getElementById("generate-button").addEventListener("click", generateRandomSentence);
 
 //----------typing effect----------------------
 
 const paragraph = document.getElementById('typing-text');
-  const text = `"Artist Poem" is an Ethereum contract that enables individuals to define the term "Artist". Participants can contribute words they believe are associated with artists, and the contract will securely store their definitions on the Blockchain permanently. 
+const text = `"Artist Poem" is an Ethereum contract that enables individuals to define the term "Artist". Participants can contribute words they believe are associated with artists, and the contract will securely store their definitions on the Blockchain permanently. 
   In theory, as a sufficient number of definitions are input by the audience, the repetition in the database will lead the artist manifesto to gradually converge toward a midpoint. However, before reaching that point, let's play and create first.`;
-  let index = 0;
+let index = 0;
 
-  function typeNextCharacter() {
-    if (index < text.length) {
-      paragraph.appendChild(document.createTextNode(text.charAt(index)));
-      index++;
-      setTimeout(typeNextCharacter, 50); // Adjust the typing speed (in milliseconds)
-    } else {
-     // Display the entire text for 5 seconds
-     setTimeout(() => {
+function typeNextCharacter() {
+  if (!paragraph) return;
+  if (index < text.length) {
+    paragraph.appendChild(document.createTextNode(text.charAt(index)));
+    index++;
+    setTimeout(typeNextCharacter, 50); // Adjust the typing speed (in milliseconds)
+  } else {
+    // Display the entire text for 5 seconds
+    setTimeout(() => {
       // Reset the index and clear the paragraph
       index = 0;
       paragraph.textContent = '';
@@ -262,7 +269,7 @@ const paragraph = document.getElementById('typing-text');
       setTimeout(typeNextCharacter, 1000); // Optional delay before restarting (in milliseconds)
     }, 5000); // 5000 milliseconds (5 seconds)
   }
-    }
+}
 
-  // Initial start
-  setTimeout(typeNextCharacter, 1000);
+// Initial start
+setTimeout(typeNextCharacter, 1000);
